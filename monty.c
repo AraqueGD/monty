9d3c8cd7cc5cfb90761973ef5c70db56811e3069
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-05 18:50:40 -0500

    [ADD]Monty Program

---
+++ monty.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+ * main - the entry point for Monty Interpreter
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+
+char *opCodeVal;
+
+int main(int argc, char **argv)
+{
+  FILE *file_fd = NULL;
+  stack_t *Stack = NULL;
+  int code_return = 0;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t nread;
+  unsigned int line_number = 0;
+  char *opCode = NULL;
+
+  // If no file monty or more than 2 parameters (file monty + other parameter)
+  if (argc < 2 || argc > 2)
+  {
+    fprintf(stderr, "USAGE: monty file\n");
+    return (EXIT_FAILURE);
+  }
+
+  // If it's not possible open file monty
+  file_fd = fopen(argv[1], "r");
+  if (file_fd == NULL)
+  {
+    fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+    return (EXIT_FAILURE);
+  }
+
+  //Initialize Stack
+  //Stack = initStack(file_fd);
+
+  //Read file bytecode monty at end
+  while ((nread = getline(&line, &len, file_fd)) != -1)
+  {
+    line_number++;
+    opCode = strtok(line, DELIMITERS);
+    if (opCode == NULL || strncmp(opCode, "#", 1) == 0)
+      continue;
+    opCodeVal = strtok(NULL, DELIMITERS);
+    opCode_Functions(&Stack, opCode, line_number);
+  }
+  fclose(file_fd);
+  return (code_return);
+}
