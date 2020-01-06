655cb13cf7f46c51176606689b697c0b04217ae8
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-05 19:40:18 -0500

    FIx

--- monty.h
+++ monty.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
 #define DELIMITERS "\n\t\r "
 
 /**
